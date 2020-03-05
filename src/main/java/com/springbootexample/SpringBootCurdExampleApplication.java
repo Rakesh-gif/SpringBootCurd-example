8cd05a27a584534ae2a9f43b996569dc41afb483
Rakesh-gif <rakesh.fsm@gmail.com>
2020-03-05 14:23:23 +0530

    Second Commit

---
+++ src/main/java/com/springbootexample/SpringBootCurdExampleApplication.java
@@ -0,0 +1,23 @@
+package com.springbootexample;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurdExampleApplication  {
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurdExampleApplication.class, args);
+		
+	
+		
+	}
+
+	
+
+}
